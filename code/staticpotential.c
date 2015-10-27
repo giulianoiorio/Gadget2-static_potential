68a82113b40a6af53c7e4971e6c7c15451610151
Giuliano <giuliano.iorio@unibo.it>
2015-10-27 16:26:20 +0100

    Add the composite potential of Piatek, 2007 and tested on the orbit of Fornax.

--- code/staticpotential.c
+++ code/staticpotential.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <math.h>
 
+#include "allvars.h"
+#include "proto.h"
+
 
 double KeplerPotential(double x, double y, double z, double mass)
 {
@@ -13,8 +16,8 @@ double KeplerPotential(double x, double y, double z, double mass)
 double *KeplerForce(double x, double y, double z, double mass)
 {
 	static double F[3];
-	double R= x*x+y*y+z*z;
-	double den=(pow(R,1.5));
+	double R2= x*x+y*y+z*z;
+	double den=(pow(R2,1.5));
 	
 	F[0]= -(mass*x)/den;
 	F[1]= -(mass*y)/den;
@@ -48,33 +51,54 @@ double *MyamotoNagaiForce(double x, double y, double z, double a, double b, doub
 	return F;
 }
 
-double LogartmicHaloPotential(double x, double y, double z, double v_h, double R_c, double q)
+double LogartmicHaloPotential(double x, double y, double z, double v_h2, double R_c, double q)
 {
 	double R2= x*x+y*y;
 	double z_cyl=z/q; 
 	
-	return -0.5*v_h*log(R2+ z_cyl*z_cyl + R_c*R_c);
+	return  ( 0.5*v_h2*log(R2+ z_cyl*z_cyl + R_c*R_c) )/(All.G);
 }
 
-double *LogartmicHaloForce(double x, double y, double z, double v_h, double R_c, double q)
+double *LogartmicHaloForce(double x, double y, double z, double v_h2, double R_c, double q)
 {
 	static double F[3];
 	double R2= x*x+y*y;
 	double z_cyl=z/q; 
 	double den=R2 + z_cyl*z_cyl + R_c*R_c;
 	
-	F[0]=-(x*v_h)/den;
-	F[1]=-(y*v_h)/den;
-	F[2]=-(z*v_h)/(den*q*q);
+	F[0]=-(x*v_h2)/(den*All.G);
+	F[1]=-(y*v_h2)/(den*All.G);
+	F[2]=-(z*v_h2)/(den*q*q*All.G);
+	
+	return F;
+}
+
+double HernquistPotential(double x, double y, double z, double mass, double rc)
+{	
+	double r= sqrt(x*x+y*y+z*z);
+	
+	return - mass/(r + rc);
+}
+
+double *HernquistForce(double x, double y, double z, double mass, double rc)
+{	
+	double r= sqrt(x*x+y*y+z*z);
+	static double F[3];
+	double cost= mass/(r*(rc+r)*(rc+r));
+	
+	F[0]=-x*cost;
+	F[1]=-y*cost;
+	F[2]=-z*cost;
 	
 	return F;
 }
 
 
-
 #ifdef EXTPOTENTIAL
 	double *Static_Force(double x, double y, double z)
-	{
+	{	
+	
+		
 		switch (EXTPOTENTIAL) 
 		{
 			case 0:
@@ -86,8 +110,55 @@ double *LogartmicHaloForce(double x, double y, double z, double v_h, double R_c,
 				return p;	
 				break;
 			}
-		
+			
+			//P07
 			case 1:
+			{	
+				//disc
+				const double Mdisc=1e11;
+				const double a=6.5;
+				const double b=0.26;
+				//bulge
+				const double Mbulge=3.4e10;
+				const double rc=0.7;
+				//halo
+				const double vh=128*128*2;
+				const double rh=12;
+				const double q=1;
+				
+				double *p;
+				static double F[3];
+				
+                
+				p=LogartmicHaloForce(x, y, z, vh, rh, q);
+				F[0]= *(p);
+				F[1]= *(p+1);
+				F[2]= *(p+2);
+				
+               
+                
+				p=HernquistForce(x, y, z, Mbulge, rc);
+				F[0]+= *(p);
+				F[1]+= *(p+1);
+				F[2]+= *(p+2);
+                
+                // printf('FOOOOrza - 0: %f %f',x,F[0]);
+				
+                
+				p=MyamotoNagaiForce(x, y, z, a, b, Mdisc);
+				F[0]+= *(p);
+				F[1]+= *(p+1);
+				F[2]+= *(p+2);
+                
+               // printf('FOOOOrza - 0: %f %f',x,F[0]);
+				
+                 
+				return F;
+				break;
+				
+			}
+			
+			case 2:
 			{
 				const double discmass=10.0;
 				const double a=1.0;
@@ -98,7 +169,7 @@ double *LogartmicHaloForce(double x, double y, double z, double v_h, double R_c,
 				break;
 			}
 			
-			case 2:
+			case 3:
 			{
 				const double vh=10.0;
 				const double rc=2.0;
@@ -108,6 +179,7 @@ double *LogartmicHaloForce(double x, double y, double z, double v_h, double R_c,
 				return p;
 				break;
 			}
+			
 		}
 	}
 	
@@ -119,6 +191,28 @@ double *LogartmicHaloForce(double x, double y, double z, double v_h, double R_c,
 			{
 				const double pointmass=1e11;
 				return KeplerPotential(x, y, z, pointmass);
+				break;
+			}
+			
+			//P07
+			case 1:
+			{
+				//disc
+				const double Mdisc=1e11;
+				const double a=6.5;
+				const double b=0.26;
+				//bulge
+				const double Mbulge=3.4e10;
+				const double rc=0.7;
+				//halo
+				const double vh=128*2;
+				const double rh=12;
+				const double q=1;
+				
+
+				
+				
+				return LogartmicHaloPotential(x, y, z, vh, rh, q) +  HernquistPotential(x, y, z, Mbulge, rc) + MyamotoNagaiPotential(x, y, z, a, b, Mdisc);
 				break;
 			}
 		
